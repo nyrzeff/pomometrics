f8e6b3d709e7f59611be33be3c5a7938d939588b
nyrghzef <nyrghzef@keemail.me>
2024-09-26 11:20:08 +0100

    fix: fix wrong variable names in main.js

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 // calculate time spent on sessions and on breaks in minutes
 function calculate() {
-   const totalFocusLength = document.getElementById("total-focus-length").valueAsNumber;
-   const sessionLength = document.getElementById("session-length").valueAsNumber;
-   const shortBreakLength = document.getElementById("short-break-length").valueAsNumber;
-   const longBreakLength = document.getElementById("long-break-length").valueAsNumber;
-   const sessionsPerCycle = document.getElementById("sessions-per-cycle").valueAsNumber;
+   const totalFocusDuration = document.getElementById("total-focus-duration").value;
+   const sessionDuration = document.getElementById("session-duration").value;
+   const shortBreakDuration = document.getElementById("short-break-duration").value;
+   const longBreakDuration = document.getElementById("long-break-duration").value;
+   const sessionsPerCycle = document.getElementById("sessions-per-cycle").value;
    const startTime = document.getElementById("start-time").value;
 
-   const endTime = calculateEndTime(startTime, totalFocusLength)
+   const endTime = calculateEndTime(startTime, totalFocusDuration)
    console.log(`end time is ${endTime}`);
 
-   const totalCycleDuration = ((sessionLength + shortBreakLength) * sessionsPerCycle) - shortBreakLength + longBreakLength;
-   const numberOfCycles = totalFocusLength / totalCycleDuration;
+   const totalCycleDuration = ((sessionDuration + shortBreakDuration) * sessionsPerCycle) - shortBreakDuration + longBreakDuration;
+   const numberOfCycles = totalFocusDuration / totalCycleDuration;
    console.log(`Cycle duration: ${totalCycleDuration}\nNumber of cycles: ${numberOfCycles}`);
 
-   const focusTimePerCycle = sessionLength * sessionsPerCycle;
+   const focusTimePerCycle = sessionDuration * sessionsPerCycle;
    const breakTimePerCycle = totalCycleDuration - focusTimePerCycle;
    console.log(`Focus time per cycle: ${focusTimePerCycle}\nBreak time per cycle: ${breakTimePerCycle}`);
 
