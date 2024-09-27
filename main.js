93b914b79854d2e468a392bfa22be23abb68dd22
nyrghzef <nyrghzef@keemail.me>
2024-09-27 21:40:54 +0100

    fix: parse input values to int (+ a slight naming improvement)

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-// calculate time spent on sessions and on breaks in minutes
 function calculate() {
-   const totalFocusDuration = document.getElementById("total-focus-duration").value;
-   const sessionDuration = document.getElementById("session-duration").value;
-   const shortBreakDuration = document.getElementById("short-break-duration").value;
-   const longBreakDuration = document.getElementById("long-break-duration").value;
-   const sessionsPerCycle = document.getElementById("sessions-per-cycle").value;
+   const totalBlockDuration = parseInt(document.getElementById("total-block-duration").value);
+   const sessionDuration = parseInt(document.getElementById("session-duration").value);
+   const shortBreakDuration = parseInt(document.getElementById("short-break-duration").value);
+   const longBreakDuration = parseInt(document.getElementById("long-break-duration").value);
+   const sessionsPerCycle = parseInt(document.getElementById("sessions-per-cycle").value);
    const startTime = document.getElementById("start-time").value;
 
-   const endTime = calculateEndTime(startTime, totalFocusDuration)
+   const endTime = calculateEndTime(startTime, totalBlockDuration)
    console.log(`end time is ${endTime}`);
 
    const totalCycleDuration = ((sessionDuration + shortBreakDuration) * sessionsPerCycle) - shortBreakDuration + longBreakDuration;
-   const numberOfCycles = totalFocusDuration / totalCycleDuration;
+   const numberOfCycles = totalBlockDuration / totalCycleDuration;
    console.log(`Cycle duration: ${totalCycleDuration}\nNumber of cycles: ${numberOfCycles}`);
 
    const focusTimePerCycle = sessionDuration * sessionsPerCycle;
