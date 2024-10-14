02ce5dd1d0ff8832d5bcadbc61180734ffb381c4
nyrghzef <nyrghzef@keemail.me>
2024-10-14 18:15:09 +0100

    feat: make session/break markers work for the first cycle

--- main.js
+++ main.js
@@ -66,57 +66,84 @@ function displayVisualization(cycleCoefficient, cycleDurationMinutes) {
   // polyline always has the same width (1000px), but can de divided in up to 192 five-minute chunks (16h max block duration)
   // helpful: where do I need to set a marker in the polyline?
   // 1000 px / 192 chunks = 5.208(3)
-  // then 5.208(3) * amount of 5min increments corresponding to a break/session/cycle
 
-  const workingWidth = 1000; // px
+  const polylineWidth = 1000; // px
   const amountOfChunks = totalBlockDurationMinutes / 5;
-  const smallestChunkIncrement = workingWidth / amountOfChunks;
+  const smallestXIncrement = polylineWidth / amountOfChunks;
 
-  createSections(smallestChunkIncrement, cycleDurationMinutes);
+  // then 5.208(3) * amount of 5min increments corresponding to a break/session/cycle
+  createSections(smallestXIncrement, cycleDurationMinutes, cycleCoefficient);
 }
 
-function createSections(smallestChunkIncrement, cycleDurationMinutes) {
+function createSections(
+  smallestXIncrement,
+  cycleDurationMinutes,
+  cycleCoefficient,
+) {
+  // chunk = 5-minute block
   let x = 0,
-    inc = 0;
+    cycleChunks = cycleDurationMinutes / 5,
+    sessionChunks = sessionDurationMinutes / 5,
+    shortBreakChunks = shortBreakDuration / 5,
+    actualMinutes = 0,
+    currentTime;
 
-  for (let i = 1; x + inc < 1000; ++i) {
-    let y1, y2;
+  for (let i = 1; x + cycleChunks < 1000; ++i) {
+    for (let j = 1; j <= sessionsPerCycle; ++j) {
+      x = sessionChunks * (smallestXIncrement * j);
 
-    inc = cycleDurationMinutes / 5;
-    x = smallestChunkIncrement * inc * i;
-    console.log(`marker at ${x}\n`);
+      console.log(`x is ${x}\tj is ${j}\n`);
 
-    y1 = 5;
-    y2 = 15;
+      actualMinutes = sessionDurationMinutes * j + shortBreakDuration * (j - 1);
+      currentTime = helper.calculateEndTime(startTime, actualMinutes);
 
-    const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    line.setAttribute("id", "line");
-    line.setAttribute("x1", x);
-    line.setAttribute("y1", y1);
-    line.setAttribute("x2", x);
-    line.setAttribute("y2", y2);
-    line.setAttribute("fill", "none");
-    line.setAttribute("stroke", "white");
-    line.setAttribute("stroke-width", "1");
+      drawLine(x, 5, 10);
+      drawText(x, 15, currentTime);
 
-    const actualMinutes = cycleDurationMinutes * i;
+      x += shortBreakChunks * (smallestXIncrement * j);
 
-    console.log(
-      `actualMinutes: ${actualMinutes}\n\nBreakdown:\n\ttotalBlockDurationMinutes: ${totalBlockDurationMinutes}\n\tx: ${x}\n\tres: ${helper.floorToMultipleOf(
-        (totalBlockDurationMinutes * x) / 960,
-        5,
-      )}\n\tres (nofloor): ${(totalBlockDurationMinutes * x) / 960}`,
-    );
+      console.log("x is " + x + smallestXIncrement * shortBreakChunks);
 
-    const currentTime = helper.calculateEndTime(startTime, actualMinutes);
+      actualMinutes += shortBreakDuration;
+      currentTime = helper.calculateEndTime(startTime, actualMinutes);
 
-    const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-    text.textContent = currentTime;
+      if (j != sessionsPerCycle) {
+        drawLine(x, 10, 15);
+        drawText(x - 1, 33, currentTime);
+      }
+    }
 
-    text.setAttributeNS(null, "x", x - 12);
-    text.setAttributeNS(null, "y", y2 - 12);
+    x = cycleChunks * (smallestXIncrement * i);
 
-    svg.appendChild(line);
-    svg.appendChild(text);
+    actualMinutes = cycleDurationMinutes * i;
+    currentTime = helper.calculateEndTime(startTime, actualMinutes);
+
+    drawLine(x, 5, 15);
+    drawText(x, 15, currentTime);
+    console.log("\n");
   }
+}
+
+function drawLine(x, y1, y2) {
+  const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+  line.setAttribute("id", "line");
+  line.setAttribute("x1", x);
+  line.setAttribute("y1", y1);
+  line.setAttribute("x2", x);
+  line.setAttribute("y2", y2);
+  line.setAttribute("fill", "none");
+  line.setAttribute("stroke", "white");
+  line.setAttribute("stroke-width", "1");
+
+  svg.appendChild(line);
+}
+
+function drawText(x, y2, currentTime) {
+  const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+  text.setAttributeNS(null, "x", x - 8.5);
+  text.setAttributeNS(null, "y", y2 - 12);
+  text.setAttribute("font-size", "7");
+  text.textContent = currentTime;
+
+  svg.appendChild(text);
 }
