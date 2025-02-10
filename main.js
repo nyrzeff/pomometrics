6cd9c106f407d75dd6a92a40af2a81c318a51cf1
nyrghzef <nyrghzef@keemail.me>
2025-02-10 17:28:48 +0000

    feat: add sliders and styles in a mobile first design approach

--- main.js
+++ main.js
@@ -1,95 +1,34 @@
 import Chart from "chart.js/auto";
-import * as helper from "./helper.mjs";
 
-const totalBlockDurationMinutes = parseInt(
-  document.getElementById("total-block-duration").value,
-);
+document.querySelectorAll(".slidercontainer").forEach((container) => {
+  const slider = container.querySelector("input");
+  const output = container.querySelector("output");
 
-const sessionDurationMinutes = parseInt(
-  document.getElementById("session-duration").value,
-);
-
-const shortBreakDuration = parseInt(
-  document.getElementById("short-break-duration").value,
-);
-
-const longBreakDuration = parseInt(
-  document.getElementById("long-break-duration").value,
-);
-
-const sessionsPerCycle = parseInt(
-  document.getElementById("sessions-per-cycle").value,
-);
-
-const startTime = document.getElementById("start-time").value;
-
-const calculateButton = document.getElementById("calculate-button");
-
-calculateButton.addEventListener("click", () => {
-  calculate();
+  slider.oninput = function () {
+    output.innerHTML = this.value;
+  };
 });
 
-function calculate() {
-  const endTime = helper.calculateEndTime(startTime, totalBlockDurationMinutes);
 
-  const cycleDurationMinutes =
-    (sessionDurationMinutes + shortBreakDuration) * sessionsPerCycle -
-    shortBreakDuration;
 
-  const cycleCoefficient = totalBlockDurationMinutes / cycleDurationMinutes;
-
-  const focusTimePerCycle = sessionDurationMinutes * sessionsPerCycle;
-  const breakTimePerCycle = cycleDurationMinutes - focusTimePerCycle;
-
-  const totalFocusTime = helper.floorToMultipleOf(
-    focusTimePerCycle * cycleCoefficient,
-    sessionDurationMinutes,
+  );
+  );
   );
 
-  const totalShortBreakTime = helper.floorToMultipleOf(
-    breakTimePerCycle * cycleCoefficient,
-    shortBreakDuration,
-  );
 
-  const totalLongBreakTime = helper.floorToMultipleOf(
-    longBreakDuration * (Math.floor(cycleCoefficient) - 1),
-    longBreakDuration,
-  );
-
-  displayVisualization(totalFocusTime, totalShortBreakTime, totalLongBreakTime);
-}
-
-function displayVisualization(
-  totalFocusTime,
-  totalShortBreakTime,
-  totalLongBreakTime,
-) {
-  const unusedTime =
-    totalBlockDurationMinutes -
-    (totalFocusTime + totalShortBreakTime + totalLongBreakTime);
 
   console.log(
-    `totalFocus: ${totalFocusTime}\ntotalShortBreak: ${totalShortBreakTime}\ntotalLongBreak: ${totalLongBreakTime}\nremainingTime: ${unusedTime}`,
   );
 
-  const doughnut = document.getElementById("breakdownDoughnut");
 
   const doughnutData = {
-    labels: ["Focus", "Long break", "Short break", "Unused time"],
     datasets: [
       {
         label: "Minutes",
-        data: [
-          totalFocusTime,
-          totalLongBreakTime,
-          totalShortBreakTime,
-          unusedTime,
-        ],
         backgroundColor: [
           "rgb(60, 97, 30)",
           "rgb(208, 50, 14)",
           "rgb(255, 99, 71)",
-          "rgb(169, 169, 169)",
         ],
         hoverOffset: 4,
       },
